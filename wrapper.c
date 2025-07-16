33ef190caad45bc448fe6c3f11e1af5129e02c9f
William1003 <52985265+William1003@users.noreply.github.com>
2025-07-16 11:49:35 +0800

    change fdt_setup_gic to 32bit

--- wrapper.c
+++ wrapper.c
@@ -225,11 +225,11 @@ int fdt_setup_gic(void *fdt, uint64_t gicd_addr, uint64_t gicc_addr,
         return 0;
     }
   }
-  fdt64_t addr[4] = {
-      cpu_to_fdt64(gicd_addr),
-      cpu_to_fdt64(0x1000),
-      cpu_to_fdt64(gicc_addr),
-      cpu_to_fdt64(0x2000),
+  fdt32_t addr[4] = {
+      cpu_to_fdt32((uint32_t)gicd_addr),
+      cpu_to_fdt32(0x1000),
+      cpu_to_fdt32((uint32_t)gicc_addr),
+      cpu_to_fdt32(0x2000),
   };
   r = fdt_setprop(fdt, node, "reg", addr, sizeof(addr));
   fdt_nop_property(fdt, node, "interrupts");
